31f5ea4ee0186716a35bf4b7a44cd7e7c2c695a5
illocarlos <carlosalbendiz93@gmail.com>
2024-04-02 20:28:05 +0200

    ok

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -15,12 +15,12 @@ const ListItem = ({ item, addItem }: ListItemProps) => {
 
         <button
 
-            className=" rounded-sm border-2 border-teal-500 w-5/6  py-2 my-2 mx-6 
+            className=" rounded-sm border-2 border-teal-500 w-5/6  py-2 my-2 mx-4
             hover:bg-teal-200 transition-all ease-linear
             "
             onClick={() => addItem(item)}
         >
-            <div className=" flex flex-row  justify-between items-center ">
+            <div className=" flex flex-row  justify-between items-center mx-2 ">
 
                 <h1 className="text-lg font-extrabold  uppercase"> {item.name}</h1>
                 <p className="text-2xl font-extrabold">+</p>
